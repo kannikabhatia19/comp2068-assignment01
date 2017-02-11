c2b92d31c80d41aa2be5b5cbb783ca52629c9d80
kannikabhatia19 <minniebhatia19@gmail.com>
2017-02-10 21:40:42 -0500

    left with services, footer, size to the height of screen

--- routes/myprojects.js
+++ routes/myprojects.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('myproject', { title: 'My Project' });
+  res.render('myprojects', { title: 'My Projects' });
 });
 
 module.exports = router;
